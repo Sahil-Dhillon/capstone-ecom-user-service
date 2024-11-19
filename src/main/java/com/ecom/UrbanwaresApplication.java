0591a0a9ccf74715b24018dcc196ce13a6077cff
Panshul Saxena <panshul.saxena@incedoinc.com>
2024-11-19 16:31:01 +0530

    adding urbanwares-user

---
+++ src/main/java/com/ecom/UrbanwaresApplication.java
@@ -0,0 +1,13 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanwaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanwaresApplication.class, args);
+	}
+
+}
